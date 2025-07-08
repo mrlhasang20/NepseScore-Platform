8cfcf97583bd83a5afff834fcbe838788e8871e1
Lhasang Lama <lamalhasang63@gmail.com>
2025-07-08 11:32:22 +0545

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ if not (os.path.exists(os.path.join(script_dir, "../data/scored_company_data.csv
     st.write("Running pipeline to generate data...")
     run_pipeline()
 
-st.title("Nepse Invest")
+st.title("NepseScore Platform")
 
 # Load data
 scored_data = pd.read_csv("data/scored_company_data.csv")
