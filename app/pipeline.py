c8a8378ef4674b77292b6fdfc2f9abdcdafb4e8b
Lhasang Lama <lamalhasang63@gmail.com>
2025-07-21 22:35:37 +0545

    Update pipeline.py

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# Adding the project root directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import subprocess
 import pandas as pd
